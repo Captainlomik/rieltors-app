af0af2f2477782fbfb8b6e7de362e456ec9ed422
captainlomik <nastiy248@mail.ru>
2024-02-09 00:54:34 +0300

    add tabs with form

--- src/app/sale/sale-page/sale-page.component.ts
+++ src/app/sale/sale-page/sale-page.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-sale-page',
   templateUrl: './sale-page.component.html',
   styleUrls: ['./sale-page.component.scss']
 })
 export class SalePageComponent {
+  title = 'Продажа'
 
+  tabs = ['Пользователь', 'Объект недвижимости', 'Предложение'];
+  select = 0
+  typeOfHousing: string = 'flat'
+
+
+  next(index: number) {
+    this.select = index + 1
+  }
+
+  previous(index: number) {
+    this.select = index - 1
+
+  }
 }
