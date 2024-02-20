c9f8e4f6e1855b4891c40c5691c2e859c81d2e5a
captainlomik <nastiy248@mail.ru>
2024-02-20 23:58:06 +0300

    add purchases

--- src/app/purchase/purchase-list/purchase-list.component.ts
+++ src/app/purchase/purchase-list/purchase-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Purchase, PurchaseObject } from 'src/app/shared/interface';
+import { PurchaseObjectService } from 'src/app/shared/services/purchaseObject.service';
 
 @Component({
   selector: 'app-purchase-list',
   templateUrl: './purchase-list.component.html',
   styleUrls: ['./purchase-list.component.scss']
 })
-export class PurchaseListComponent {
+export class PurchaseListComponent implements OnInit {
+  title = 'Потребности объекты'
+  purchases$: Observable<PurchaseObject[]> | undefined
+
+  constructor(private purchaseObjectService: PurchaseObjectService) {
+  }
+  ngOnInit(): void {
+    this.getPurchase()
+  }
+
+  getPurchase() {
+    this.purchases$ = this.purchaseObjectService.get()
+  }
+
 
 }
+
