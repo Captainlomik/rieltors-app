328f3d1362f5781ba93e1aa59ce8816868ba5f2c
captainlomik <nastiy248@mail.ru>
2024-02-29 18:34:09 +0300

    add purchase

--- src/app/client/client-detail-page/client-detail-page.component.ts
+++ src/app/client/client-detail-page/client-detail-page.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { ClientService } from '../../shared/services/client.service';
-import { Observable, map, mergeMap, switchMap } from 'rxjs';
-import { Client, Flat, House, Land, Object, Offer } from '../../shared/interface';
+import { Observable, filter, map, mergeMap, switchMap } from 'rxjs';
+import { Client, Flat, House, Land, Object, Offer, Purchase, PurchaseObject } from '../../shared/interface';
 import { Location } from '@angular/common';
 import { OffersService } from 'src/app/shared/services/offers.service';
 import { ObjectService } from 'src/app/shared/services/object.service';
+import { PurchaseService } from 'src/app/shared/services/purchase.service';
+import { PurchaseObjectService } from 'src/app/shared/services/purchaseObject.service';
 
 @Component({
   selector: 'app-client-detail-page',
@@ -15,14 +17,18 @@ import { ObjectService } from 'src/app/shared/services/object.service';
 export class ClientDetailPageComponent implements OnInit {
   param!: number
   client$!: Observable<Client>
-  offer: Offer | undefined
+  offer!: Offer
+  purchase!: Purchase
   arrayObjects: Array<Flat | House | Land> = []
+  arrayPurchaseObject: PurchaseObject[] = []
 
   constructor(private route: ActivatedRoute,
     private clientService: ClientService,
     private location: Location,
     private offerService: OffersService,
-    private objectService: ObjectService) { }
+    private objectService: ObjectService,
+    private purchaseService: PurchaseService,
+    private purchaseObjectService: PurchaseObjectService) { }
 
   ngOnInit(): void {
     this.client$ = this.route.params.pipe(switchMap((params: Params) => {
@@ -31,6 +37,7 @@ export class ClientDetailPageComponent implements OnInit {
     }))
 
     this.getObject()
+    this.getPurchase()
   }
 
   goBack(): void {
@@ -55,4 +62,26 @@ export class ClientDetailPageComponent implements OnInit {
         this.offer = el
     )
   }
+
+  getPurchase() {
+    this.purchaseService.get().pipe(
+      map(data => {
+        return data.filter((object: Purchase) => {
+          return +object.clientId === +this.param
+        })
+      }),
+      mergeMap(res => {
+        for (let i of res) {
+          this.purchaseObjectService.get_byId(i.purchaseObjectId).subscribe(el => {
+            if (el) {
+              this.arrayPurchaseObject.push(el)
+            }
+          })
+        } return res
+      })
+    ).subscribe(
+      el => this.purchase = el
+    )
+  }
+
 }
